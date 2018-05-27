169f2f019ba195d5cceba4bd9c7819f1cf5fd1a2
Kevin Cheung <kcheung96@gmail.com>
2018-05-27 19:28:22 -0400

    connectmechatapplication

---
+++ public/js/script.js
@@ -0,0 +1,96 @@
+
+var chatBody = document.querySelector('#chat');
+$(function(){
+	var socket = io.connect();
+	var $messageForm = $('#messageForm');
+	var $message = $('#message');
+	var $chat = $('#chat');
+	var $messageArea = $('#messageArea');
+	var $userFormArea = $('#userFormArea');
+	var $userForm = $('#userForm');
+	var $users = $('#users');
+	var $username = $('#username');
+	var $password = $('#password');
+	var $userError = $('#userError');
+
+	$messageForm.submit(function(e){
+		e.preventDefault(); 
+		socket.emit('send message', $message.val(), function(data){
+			var date = formatAMPM(new Date());
+			$chat.append('<div class="alert alert-success"><strong>'+"[" + date + "]</strong>" + data + '</div>');
+			chatBody.scrollTop = chatBody.scrollHeight - chatBody.clientHeight;	
+		});
+		$message.val('');
+	});
+
+	socket.on('new message', function(data){
+		var date = formatAMPM(new Date());
+		$chat.append('<div class="alert alert-info"><strong>'+"[" + date + "] " +data.user+'</strong>: '+data.msg+'</div>');
+		chatBody.scrollTop = chatBody.scrollHeight - chatBody.clientHeight;
+	});
+
+	socket.on('whisper', function(data){
+		var date = formatAMPM(new Date());
+		$chat.append('<div class="alert alert-success"><strong>'+"[" + date + "] From " +data.user+'</strong>: '+data.msg+'</div>');	
+		chatBody.scrollTop = chatBody.scrollHeight - chatBody.clientHeight;				
+	});
+
+	$userForm.submit(function(e){
+		e.preventDefault(); 
+		socket.emit('new user', $username.val(), function(data){
+			var date = formatAMPM(new Date());
+			if(data){
+				$userFormArea.hide();
+				$messageArea.show();
+			}
+			else{
+				$userError.show();
+			}
+		});
+		$username.val('');
+	});
+
+	socket.on('get users', function(data){
+		var html = '';
+		for(i = 0; i < data.length; i++){
+			html += '<li class="list-group-item">' + data[i] + '</li>';
+		}
+		$users.html(html);
+	});
+
+	socket.on('load messages', function(data){
+		for(i=0; i < data.length; i++){
+			chatBody.scrollTop = chatBody.scrollHeight - chatBody.clientHeight;	
+			$chat.append('<div class="alert alert-danger"><strong>'+"[" + data[i].date + "] " + data[i].from + "</strong>: " + data[i].message + ' </div>');	
+		}
+			console.log("Finished loading old messages.");
+	});
+
+	$('#message').keypress(function (e) {
+			if (e.which == 13) {
+			$('#submit').submit();
+			return false;    //<---- Add this line
+			}
+	});
+
+});
+
+
+// socket.on('display message', function(data){
+// 	displayMessages(data);
+// });
+
+
+function formatAMPM(date) {
+	var month = date.getUTCMonth() +1;
+	var day = date.getUTCDate();
+	var year = date.getUTCFullYear();
+	var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = month+"/"+day + "/" + year + ' ' + hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+} 
---
+++ server.js
@@ -0,0 +1,175 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app); //creates server
+var io = require('socket.io').listen(server);
+const mysql = require('mysql');
+
+
+users = {};
+connections = [];
+var ret = [];
+app.use(express.static('public'));
+
+<<<<<<< HEAD
+var port = process.env.PORT || 5000
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+=======
+const db = mysql.createConnection({
+<<<<<<< HEAD
+		host     : 'localhost',
+		user     : 'root',
+		password : 'localhost2341',
+		database : 'connectme'
+=======
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b2688ca46574e6',
+    password: '5ac14581',
+    database: 'heroku_48febb90a6d362c'
+>>>>>>> d26ec13d63745ed5fe0f86c75e36e8a5f4b109f8
+>>>>>>> aeed7ab28d0f82bea3991530b918176d173b418a
+});
+
+db.connect((err) => {
+	if (err) throw err;
+	console.log("SQL server connected...");
+});
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+	// let sql = 'SELECT * FROM messages';
+	// let query = db.query(sql, (err, results) => {
+	// 	if(err) throw err;
+	// 	console.log(results);
+	// 	res.send('Posts fetched...');
+	// });
+
+});
+
+<<<<<<< HEAD
+const db = mysql.createConnection({
+    host	: process.env.DB_HOST,
+    user	: process.env.DB_USER,  
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+=======
+server.listen(process.env.PORT || 5000, '0.0.0.0', function(){
+	console.log('Listening to port: ' + 5000)
+>>>>>>> d26ec13d63745ed5fe0f86c75e36e8a5f4b109f8
+});
+
+console.log('Server is running...');
+
+io.sockets.on('connection', function(socket){
+<<<<<<< HEAD
+	var sql = '(SELECT * FROM connectme.messages ORDER BY idmessages DESC LIMIT 6) ORDER BY idmessages ASC;'
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+	var sql = '(SELECT * FROM connectme.messages ORDER BY idmessages DESC LIMIT 6) ORDER BY idmessages ASC;'
+=======
+	var sql = 'SELECT * FROM heroku_48febb90a6d362c.messages LIMIT 10;'
+>>>>>>> f0f7fd4b44ad92b340d43dd5577a24e71a10dcbe
+=======
+	var sql = 'SELECT * FROM heroku_48febb90a6d362c.messages LIMIT 10;'
+>>>>>>> f0f7fd4b44ad92b340d43dd5577a24e71a10dcbe
+=======
+	var sql = '(SELECT * FROM heroku_48febb90a6d362c.messages ORDER BY idmessages DESC LIMIT 6) ORDER BY idmessages ASC;'
+>>>>>>> d26ec13d63745ed5fe0f86c75e36e8a5f4b109f8
+>>>>>>> aeed7ab28d0f82bea3991530b918176d173b418a
+	db.query(sql, function(err, rows, fields) {
+		if (err) throw err;
+		else {
+			// for (var i in rows) {
+   //  			ret.push(rows[i]);;
+			// }
+			socket.emit('load messages', rows);
+    		console.log('Old messages saved.');
+		}
+	});
+
+	connections.push(socket);
+	console.log('Users online: '+ connections.length);
+
+	//disconnect
+	socket.on('disconnect', function(data){
+		delete users[socket.username];
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Users online: ' +  connections.length);
+	});
+	
+	//send messages
+	socket.on('send message', function(data, callback){
+		var msg = data.trim();
+		//console.log(data);
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			var index = msg.indexOf(' ');
+			if(index !== -1){
+				var name = msg.substr(0,index);
+				var msg = msg.substr(index + 1);
+				if(name in users){
+					users[name].emit('whisper', {msg: msg, user: socket.username});
+					callback("<strong> To " + name + ": </strong>" + msg);
+					console.log("Whispering continues...");
+				}else{
+					callback(" Enter a valid username! (CASE SENSITIVE)");
+				}
+			}else{
+				callback(' Please enter a message for your whisper!');
+			}
+
+		}else{
+			var date = formatAMPM(new Date());
+			let post = {message:msg, from:socket.username, date:date};
+			var sql = 'INSERT INTO messages SET ?';
+			db.query(sql, post, function (err, result){
+			if(err) throw err;
+				console.log("1 record inserted!");
+			});
+			io.sockets.emit('new message', {msg: msg, user: socket.username});
+		}
+	});
+
+	//New User
+	socket.on('new user', function(data, callback){
+		if(data in users || data.indexOf(" ") != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.username = data;
+			users[socket.username] = socket;
+			//users.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+
+	//Update Usernames
+	function updateUsernames(){
+		io.sockets.emit('get users', Object.keys(users));
+	}
+});
+
+
+	function formatAMPM(date) {
+		var month = date.getUTCMonth() +1;
+		var day = date.getUTCDate();
+		var year = date.getUTCFullYear();
+		var hours = date.getHours();
+	    var minutes = date.getMinutes();
+	    var ampm = hours >= 12 ? 'PM' : 'AM';
+	    hours = hours % 12;
+	    hours = hours ? hours : 12; // the hour '0' should be '12'
+	    minutes = minutes < 10 ? '0'+minutes : minutes;
+	    var strTime = month+"/"+day + "/" + year + ' ' + hours + ':' + minutes + ' ' + ampm;
+	    return strTime;
+	} 
+
+// 404 handler
+app.use(function(req,res) {
+    res.status(404).send('Dunno');
+})
